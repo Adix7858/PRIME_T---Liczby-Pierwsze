96c2d8f67580fcfdcfdfc35dc57425258c3fc6eb
Adix7858 <82727723+Adix7858@users.noreply.github.com>
2021-04-17 20:52:19 +0200

    Add files via upload
    Rozruch z SPOJ

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int n=1; n<100000; n++)
+    {
+        if(n%2==0)
+        {
+            cout<<"TAK"<<endl;
+        }
+        cout<<"NIE"<<endl;
+    }
+    return 0;
+}
