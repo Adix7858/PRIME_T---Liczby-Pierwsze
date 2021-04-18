6ec933ef1d12f043f254c191c9b09cf5f0c12792
Adix7858 <82727723+Adix7858@users.noreply.github.com>
2021-04-18 17:35:47 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
-#include <iostream>
 
-using namespace std;
+ #include <iostream>
 
-int main()
-{
-    for(int n=1; n<100000; n++)
+    using namespace std;
+
+    int liczba,dziel;
+
+    int main()
     {
-        if(n%2==0)
-        {
-            cout<<"TAK"<<endl;
+        for (int i=1; i<=100000; i++)
+        {   cout<<"Podaj liczbe do sprawdzenia:";
+            cin>>liczba;
+            if   (liczba<=2) cout<<"NIE"<<endl;
+            else if (liczba==2) cout<<"NIE";
+            else
+            {
+            for (int i=2; i<liczba; i++)
+            {
+                dziel=liczba%i;
+
+                if(dziel==0)  {cout<<"NIE"<<endl; break;}
+            }
+                if(dziel!=0)   cout<<"TAK"<<endl;
+            }
         }
-        cout<<"NIE"<<endl;
+
+        return 0;
     }
-    return 0;
-}
